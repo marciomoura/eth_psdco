9cb6daa35c2c9be7f74b949e755c6733bada1130
Marcio Moura Bridon Junior <marcio.moura-bridon-junior@ch.abb.com>
2021-03-23 09:06:27 +0100

    push code into /src folder

---
+++ src/f_SE_NR_algorithm_v2021.m
@@ -0,0 +1,108 @@
+function [ V, theta, eps_all, time, convergence, it_num ] = f_SE_NR_algorithm_v2021 ( V, theta, topo, Y_bus, z, W, Wsqrt, ...
+    ind_meas, N_meas, eps_tol, Max_iter, H_decoupled, H_sparse, linsolver )
+%**************************************************************************
+%DESCRIPTION OF FUNCTION (VERSION 2021)
+%**************************************************************************
+%This function solves the State Estimation problem cast as a WLS problem
+%using the Gauss-Newton (GN) method. The possible solution options include
+%choosing different algorithms for solving a linear system at each
+%iteration as well as using either a full GN method or fast decoupled GN
+%method.
+
+%INPUTS
+%V           - vector of voltage magnitudes of ALL buses in the system
+%theta       - vector of voltage phase angles for ALL buses in the system
+%topo        - structure containing the topology of the system and
+%              parameters of system elements. Used for computing H and h
+%Y_bus       - structure that contains real and imag parts of admittances
+%              of structurally nonzero elements in the admittance matrix
+%z           - vector containing all available measurements in the system
+%W           - diagonal matrix containing weights assigned to measurements
+%Wsqrt       - diagonal matrix, elements are square roots of elements of W   
+%ind_meas    - structure that for each type of measurement contains info
+%              on what are the indexes of available measurements in a list
+%              of all possible measurements for this type
+%N_meas      - structure that contains the number of available measurements
+%              for each type of measurements 
+%eps_tol     - stopping criterion. GN should stop if max(|delta_x|)<eps_tol
+%Max_iter    - stopping criterion, maximum number of GN iterations
+%H_decoupled - control variable. If 0, full Jacobian is constructed and 
+%              'full' GN algorithm is used. If 1, decoupled Jacobian is 
+%              constructed and fast decoupled algorithm is used
+%H_sparse    - control variable. If 1, matrix H will be in a sparse form. 
+%              If 0, matrix H will be in a dense form
+%linsolver   - control variable for choosing a linear system solver:
+%              1 - matrix inverse of G
+%              2 - Cholesky factorization of G (done by backslash)
+%              3 - QR decomposition of H with minimum number of fill-ins
+%              4 - Hybrid method (decompose H by QR, but only compute R)
+
+%OUTPUTS
+%V           - vector of voltage magnitudes of ALL buses in the system
+%theta       - vector of voltage phase angles of ALL buses in the system
+%eps_all     - vector, each element of which is equal to max(|delta_x|) at
+%              a certain iteration. Shows the convergence pattern
+%time        - total computation time for all iterations for solving linear
+%              system. Time for computing H and h should NOT be included here
+%convergence - if 1, GN has converged to a solution, otherwise 0
+
+%**************************************************************************
+%FUNCTION CODE
+%**************************************************************************
+
+
+%STUDENT CODE
+%for computing matrix H and vector h, use the following functions:
+
+eps_all = 0;
+time = 0;
+convergence = 0;
+it_num = 0;
+
+for i = 1 : Max_iter
+    [ H ] = f_measJac_H_v2021( V, theta, Y_bus, topo, ind_meas, N_meas, H_decoupled, H_sparse);
+    [ h ] = f_measFunc_h_v2021( V, theta, Y_bus, topo, ind_meas, N_meas);
+   
+   
+    tic;
+    switch linsolver
+        case 1 % Direct inverse
+            % 4. Compute G(x(k))
+            G = H' * W * H;  
+            % 5. Compute right hand side g(x(k))
+            g = -(H')*W*(z-h);
+            dx = -inv(G)*g;
+        case 2 % Cholesky
+            % 4. Compute G(x(k))
+            G = H' * W * H;  
+            % 5. Compute right hand side g(x(k))
+            g = -(H')*W*(z-h);
+            dx = -G\g;
+        case 3 % Orthogonal Decomposition
+            Htilda = Wsqrt*H;
+            [Q,R,e]=qr(Htilda,0);
+            b = Wsqrt*(z-h);
+            dx(e,:) = R\(Q\b);
+        case 4 % Hybrid
+            Htilda = Wsqrt*H;
+            p=colamd(Htilda);
+            R=qr(Htilda(:,p),0);
+            b = Htilda' * Wsqrt * (z-h);
+            dx(p,:) = R'*R\(b(p,:)) ;   
+        otherwise
+    end;
+    time = time + toc;
+    
+    if max(abs(dx)) <= eps_tol    
+        convergence = 1;
+        eps_all = abs(dx);
+        it_num = i;
+        break;
+    else
+        % [ Theta; V ] - First index is Bus 1 reference.
+        theta(2:size(theta,1)) = theta(2:size(theta,1)) + dx(1:size(theta,1)-1,1);
+        V = V + dx(size(theta,1):size(dx,1),1);
+    end;
+end;
+end
+
---
+++ src/main_script_student_part1_v2021.m
@@ -0,0 +1,111 @@
+clc;clear;
+%LOADING THE DATA
+%load GoodMeasurement_14_bus.mat;
+load GoodMeasurement_1354_bus.mat;
+
+%HERE COME USER-DEFINED PARAMETERS OF GN algorithm
+eps_tol=10^-5;  %stopping criterion for max(abs(delta_x))
+Max_iter=100; %maximum number of GN iterations
+H_decoupled=0; %if 0, full H and 'normal' GN are used. If 1, decoupled H and fast decoupled GN are used
+H_sparse=0; %if 1, H is created as a sparse matrix, if 0 - as a dense matrix
+linsolver=4;  %1 - matrix inverse, 2 - Cholesky, 3 - QR, 4 - Hybrid
+
+%SOLTUION ALGORITHM STARTS HERE
+%getting Jacobian sparsity pattern (done only once)
+[topo, ind_meas, N_meas]=f_meas_indices_and_H_sparsity_pattern_v2021(topo, meas);
+
+%obtaining the admittance matrix in the compressed row form (done only once)
+Y_bus = f_Y_bus_compressed_v2021(topo);
+
+%constructing vector z, which is a vector of available measurements (hint: use structure ind_meas)
+%NOTE: different types of measurements should be in the same order as in the handout
+%STUDENT CODE 1
+fields = fieldnames(meas);
+z=[];
+for i = 1:numel(fields)
+    if ~contains(fields{i},'warning') && ~contains(fields{i},'std')
+        z = [z;meas.(fields{i})];
+        z = z(~isnan(z));
+    end;
+end;
+ 
+% %constructing the matrix of weights W and its square root Wsqrt using standard deviations provided in structure 'meas'
+% %NOTE: matrices W and Wsqrt must be constructed in sparse format!!!
+% %STUDENT CODE 2
+fields = fieldnames(meas);
+all_std=[];
+all_msr=[];
+
+% Get all std deviation elements + measurements in two different vectors
+for i = 1:numel(fields)
+  if ~contains(fields{i},'warning')
+      if contains(fields{i},'std')
+        all_std = [all_std; meas.(fields{i})];
+      else
+        all_msr = [all_msr; meas.(fields{i})];
+      end;
+  end;
+end
+
+% Fill all 'NaN' elements (from measurement vector) in the std deviation
+% vector
+for i = 1 : size(all_msr,1)
+    if isnan(all_msr(i))
+        all_std(i) = NaN;
+    end;
+end;
+
+% Remove all nodes which are not measured (filter NaN)
+msr_std = all_std(~isnan(all_std));
+
+% Diagonal elements of W is given by 1/(std^2)
+W = inv(diag(msr_std.^2));
+Wsqrt = sqrt(W);
+ 
+% %choosing the initial point (use flat start)
+% %STUDENT CODE 3
+V = ones(topo.nBus,1);
+theta= zeros(topo.nBus,1); % bus 1 is used as reference
+ 
+% %TASK 1: SOLVING SE PROBLEM WITH GN ALGORITHM
+% %STUDENT CODE 4
+% %NOTE: use the following function (the description of its inputs and
+% outputs can be found inside the function)
+[ V, theta, eps_all, time, convergence, it_num ] = f_SE_NR_algorithm_v2021 ( V, theta, topo, Y_bus, z, W, Wsqrt, ...
+            ind_meas, N_meas, eps_tol, Max_iter, H_decoupled, H_sparse, linsolver );
+         
+% tot_time = 0;
+% for n = 1 : 1
+%     V = ones(topo.nBus,1);
+%     theta= zeros(topo.nBus,1);
+%     [ V, theta, eps_all, time, convergence, it_num ] = f_SE_NR_algorithm_v2021 ( V, theta, topo, Y_bus, z, W, Wsqrt, ...
+%              ind_meas, N_meas, eps_tol, Max_iter, H_decoupled, H_sparse, linsolver );
+%     tot_time = tot_time + time;
+% end;
+% avg_time = tot_time/1;
+% fprintf('Total time: %.6f s\n', tot_time);
+% fprintf('Average time: %.6f s\n', avg_time);
+% fprintf('Number iterations: %d \n', it_num);
+
+
+% 
+% 
+% 
+% %TASK 2: COMPUTING CONDITION NUMBERS AND DENSITY FACTORS OF MATRICES
+% %STUDENT CODE 5
+% 
+% 
+% 
+% 
+% %TASK 3: COMPARING PERFORMANCE OF FULL/DECOUPLED GN
+% %STUDENT CODE 6
+% %NOTE: use the following function (the description of its inputs and
+% %outputs can be found inside the function)
+% [ V, theta, eps_all, time, convergence ] = f_SE_NR_algorithm_v2021 ( V, theta, topo, Y_bus, z, W, Wsqrt, ...
+%             ind_meas, N_meas, eps_tol, Max_iter, H_decoupled, H_sparse, linsolver );
+% 
+
+
+
+
+
